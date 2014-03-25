c9e419970c211bf481eefff2cfc097791c0e1d3b
Emil Ljungdahl <emil@kratern.se>
2014-03-25 21:08:34 +0100

    Changed addresses to reflect new domain openambit.org

--- src/openambit/mainwindow.cpp
+++ src/openambit/mainwindow.cpp
@@ -213,7 +213,7 @@ void MainWindow::showAbout()
 {
     QMessageBox::about(this, tr("About %1").arg(QCoreApplication::applicationName()),
                        tr("<h2>%1</h2><b>Version %2</b><br />Using Qt %3").arg(QCoreApplication::applicationName()).arg(QCoreApplication::applicationVersion()).arg(QString(qVersion())) +
-                       "<br /><br /><a href=\"https://github.com/openambitproject/openambit/\">https://github.com/openambitproject/openambit/</a>");
+                       "<br /><br /><a href=\"http://openambit.org/\">http://openambit.org</a>");
 }
 
 void MainWindow::settingsSaved()
