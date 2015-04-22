79559003c83fed01d2f8835285c86d6393539c2b
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2015-04-22 19:09:34 +0900

    Fix `run.sh` to match updated `README.rst`

--- run.sh
+++ run.sh
@@ -4,7 +4,20 @@ SOURCE_LOCATION="`dirname \"$0\"`"
 SOURCE_LOCATION="`( cd \"$SOURCE_LOCATION\" && pwd )`"
 
 cd $SOURCE_LOCATION
-echo "------running openambit------"
-LD_LIBRARY_PATH=./libambit-build ./openambit-build/openambit
 
+application=openambit
+if test -n "$1"; then
+     application=$1
+     shift
+fi
+case "$application" in
+    openambit)          builddir=$application-build;;
+    ambitconsole)       builddir=example-build;;
+    *)
+	echo "$application: not supported" >&2
+	exit 1
+	;;
+esac
 
+echo "------running $application------"
+LD_LIBRARY_PATH=./libambit-build ./$builddir/$application
