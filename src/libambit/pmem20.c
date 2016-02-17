65e54ae46d5c6ccbb5dc89a26ceaa199a73b6597
Johan Svärd <superdupersvard@gmail.com>
2016-02-17 12:31:35 +0100

    Clean up for debug prints.

--- src/libambit/pmem20.c
+++ src/libambit/pmem20.c
@@ -1098,22 +1098,14 @@ static int write_data_chunk(ambit_object_t *object, uint32_t address, size_t buf
         }
 
         if (libambit_protocol_command(object, ambit_command_data_write, send_data, send_data_len, &reply, &replylen, 0) == 0) {
-            static int itt = 0;
-            if ( itt < 10) {
-//                printf("ambit_command_data_write\n");
-                int i, j = 1;
-                for (i = 0; i < send_data_len; i++) {
-                    printf("%.2x", send_data[i]);
-//                    if (j > 15) {
-//                        printf("\n");
-//                        j = 0;
-//                    }
-                    j++;
-                }
-                printf("\n");
-            }
-            itt++;
             ret = 0;
+
+            // Debug output of written data.
+            int i;
+            for (i = 0; i < send_data_len; i++) {
+                printf("%.2x", send_data[i]);
+            }
+            printf("\n");
         }
 
         free(send_data);
