1dc16472892e1c9460fd69f480fe64124b51cc62
Dominik Stadler <dominik.stadler@gmx.at>
2019-11-10 09:00:47 +0100

    test.sh: Add building all hidapi variants and use adjust TMP directory

--- test.sh
+++ test.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-set -e
+set -eu
+
+# clean before building
+rm -rf *-build
+
+# first build with default option
+BUILD_EXTRAS=1 ./build.sh
+
+# then build in various ways
+for b in Debug Release;do
+  # not testable here: mac windows
+	for hid in libudev libusb pcapsimulate;do
+    rm -rf *-build
+    BUILD_EXTRAS=1 HIDAPI_DRIVER=${hid} ./build.sh -DCMAKE_BUILD_TYPE=${b}
+  done
+done
 
 # ensure that openambit2gpx.py works
 
-python2.7 tools/openambit2gpx.py test-data/testlog.log "$TMP"/testlog.gpx
+python2.7 tools/openambit2gpx.py test-data/testlog.log "${TMP:-/tmp}"/testlog.gpx
 
-diff --ignore-all-space test-data/testlog.gpx "$TMP"/testlog.gpx
+diff --ignore-all-space test-data/testlog.gpx "${TMP:-/tmp}"/testlog.gpx
 
-python3 tools/openambit2gpx.py test-data/testlog.log "$TMP"/testlog.gpx
+python3 tools/openambit2gpx.py test-data/testlog.log "${TMP:-/tmp}"/testlog.gpx
 
-diff --ignore-all-space test-data/testlog.gpx "$TMP"/testlog.gpx
+diff --ignore-all-space test-data/testlog.gpx "${TMP:-/tmp}"/testlog.gpx
